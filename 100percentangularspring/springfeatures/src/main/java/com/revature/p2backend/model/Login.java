1cdf4c6c4cc25617a76d2b686c0a742d8104ed39
TheBillionaires <102680541+TheBillionaires@users.noreply.github.com>
2022-04-11 09:42:53 -0700

    Add files via upload

---
+++ 100percentangularspring/springfeatures/src/main/java/com/revature/p2backend/model/Login.java
@@ -0,0 +1,19 @@
+package com.revature.p2backend.model;
+
+import lombok.Data;
+
+@Data
+public class Login {
+
+	private String email;
+	private String password;
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
